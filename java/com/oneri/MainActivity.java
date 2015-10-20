f6992da6e5ec1f9192902fa302bd4e4220ba3ee0
Lenthy <flenthy@telecom-paristech.fr>
2015-10-20 15:24:29 +0200

    my first commit

--- java/com/oneri/MainActivity.java
+++ java/com/oneri/MainActivity.java
@@ -1,37 +1,37 @@
 package com.oneri;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        
+        
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+       
+        actionBar.setDisplayShowTitleEnabled(true);
+        
+        /** Creating ANDROID Tab */
+        Tab tab = actionBar.newTab()
+        		.setText("Book")
+        		.setTabListener(new CustomTabListener<BookFragment>(this, "book", BookFragment.class));
+        
+        actionBar.addTab(tab);
+        
+        
+        /** Creating APPLE Tab */
+        tab = actionBar.newTab()
+        		.setText("Movie")
+        		.setTabListener(new CustomTabListener<MovieFragment>(this, "movie", MovieFragment.class));
+        
+        actionBar.addTab(tab);            
+        
+        
     }
 }
