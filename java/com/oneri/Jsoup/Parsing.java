797fc94c09f381d1c97440d82a51d86c447da728
leroyq <quentin.n.leroy@gmail.com>
2016-01-18 21:05:56 +0100

    Parsage des livres fonctionnel (avec iblist)

--- java/com/oneri/Jsoup/Parsing.java
+++ java/com/oneri/Jsoup/Parsing.java
@@ -161,6 +161,81 @@ public class Parsing {
         return imdb_image_url;
     }
 
+    public static String getFirstURLIblistSearchResult(String title){
+        String title_url_encoded = URLEncoder.encode(title);
+        String url = "http://www.iblist.com/search/search.php?item=" + title_url_encoded + "&submit=Search";
+        String first_url_iblist_search_result = "";
+        Document doc;
+        try{
+            doc = Jsoup.connect(url).get();
+            Element link = doc.select("tbody").select("li").get(0).select("a").get(0);
+            first_url_iblist_search_result = link.attr("href");
+        }catch(IOException e){
+            return "IOException in Jsoup.connect(url).get() getFirstURLIblistSearchResult title_url_encoded : " +
+                    title_url_encoded + " url : " + url;
+        }
+        return first_url_iblist_search_result;
+    }
+
+    public static String getIblistTitleName(String url){
+        Document doc;
+        String iblist_title_name = "";
+        try{
+            doc = Jsoup.connect(url).get();
+            Element link = doc.select("div[id=main]").select("div[class=boxbody]").get(0).select("td[class=content").get(0).
+                    select("td[class=content").get(0).select("table[class=main]").select("tbody").select("tbody").select("tr").
+                    select("td").select("a").get(0);
+            iblist_title_name = link.text();
+        }catch(IOException e){
+            return "IOException in Jsoup.connect(url).get() getIblistTitleName url : " + url;
+        }
+        return iblist_title_name;
+    }
+
+    public static String getIblistAuthor(String url){
+        Document doc;
+        String iblist_author = "";
+        try{
+            doc = Jsoup.connect(url).get();
+            Element link = doc.select("div[id=main]").select("div[class=boxbody]").get(0).select("td[class=content").get(0).
+                    select("td[class=content").get(0).select("table[class=main]").select("tbody").select("tbody").select("tr").
+                    select("td").select("a").get(1);
+            iblist_author = link.text();
+        }catch(IOException e){
+            return "IOException in Jsoup.connect(url).get() getIblistAuthor url : " + url;
+        }
+        return iblist_author;
+    }
+
+    public static String getIblistDescription(String url){
+        Document doc;
+        String iblist_description = "";
+        try{
+            doc = Jsoup.connect(url).get();
+            Element link = doc.select("div[id=main]").select("div[class=boxbody]").get(0).select("td[class=content").
+                    select("div[class=indent]").get(0);
+            iblist_description = link.text();
+        }catch(IOException e){
+            return "IOException in Jsoup.connect(url).get() getIblistDescription url : " + url;
+        }
+        return iblist_description;
+    }
+
+    public static String getIblistImageURL(String url){
+        Document doc;
+        String iblist_image_url = "";
+        String iblist_base_url = "http://iblist.com/";
+        try{
+            doc = Jsoup.connect(url).get();
+            Element link = doc.select("div[id=main]").select("div[class=boxbody]").get(1).select("tbody").
+                    select("img[src]").get(0);
+            iblist_image_url = iblist_base_url + link.attr("src");
+        }catch(IOException e){
+            return "IOException in Jsoup.connect(url).get() getIblistImageURL url : " + url;
+        }
+        return iblist_image_url;
+    }
+
     //Deprecated
     public static String getSmallIMDBDescription(String title){
         title = URLEncoder.encode(title).toLowerCase();
@@ -237,25 +312,39 @@ public class Parsing {
 
             String user_input_contentType = urls[1];
 
-            String imdb_url_content = getFirstURLIMDBSearchResult(user_input);
+            String title = "title";
+            String creator = "creator";
+            String image_url = "image url";
+            String description = "description";
 
-            String title = getIMDBTitleName(imdb_url_content);
-
-            String creator = "creator encule";
             Log.i("SALUTSALUT", "*" + user_input_contentType + "*");
             Log.i("SALUTSALUT", "*" + GlobalVars.APP_CONTEXT.getResources().getStringArray(R.array.content_types)[0] + "*");
 
-            if(user_input_contentType.equals(GlobalVars.APP_CONTEXT.getResources().getStringArray(R.array.content_types)[0]))
+            if(user_input_contentType.equals(GlobalVars.APP_CONTEXT.getResources().getStringArray(R.array.content_types)[0])) {
+                String imdb_url_content = getFirstURLIMDBSearchResult(user_input);
+                title = getIMDBTitleName(imdb_url_content);
                 creator = getIMDBMovieDirector(imdb_url_content);
-            if(user_input_contentType.equals(GlobalVars.APP_CONTEXT.getResources().getStringArray(R.array.content_types)[4]))
+                description = getIMDBSmallDescription(imdb_url_content);
+                image_url = getIMDBImageURL(imdb_url_content);
+            }
+            if(user_input_contentType.equals(GlobalVars.APP_CONTEXT.getResources().getStringArray(R.array.content_types)[1])) {
+                String iblist_url_content = getFirstURLIblistSearchResult(user_input);
+                title = getIblistTitleName(iblist_url_content);
+                creator = getIblistAuthor(iblist_url_content);
+                description = getIblistDescription(iblist_url_content);
+                image_url = getIblistImageURL(iblist_url_content);
+            }
+            if(user_input_contentType.equals(GlobalVars.APP_CONTEXT.getResources().getStringArray(R.array.content_types)[4])) {
+                String imdb_url_content = getFirstURLIMDBSearchResult(user_input);
+                title = getIMDBTitleName(imdb_url_content);
                 creator = getIMDBTvSeriesCreator(imdb_url_content);
+                description = getIMDBSmallDescription(imdb_url_content);
+                image_url = getIMDBImageURL(imdb_url_content);
+            }
 
 
-            String small_description = getIMDBSmallDescription(imdb_url_content);
 
-            String image_url = getIMDBImageURL(imdb_url_content);
-
-            String[] to_returns = {title, creator,image_url, small_description};
+            String[] to_returns = {title, creator,image_url, description};
 
             return to_returns;
         }
